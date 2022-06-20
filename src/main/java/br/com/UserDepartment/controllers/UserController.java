2af392513140334434c5e0dce1e144da5cc0a060
Gabriel <gabriel.c.zvarum@gmail.com>
2022-06-20 00:54:36 -0300

    Criando metodos controllers

--- src/main/java/br/com/UserDepartment/controllers/UserController.java
+++ src/main/java/br/com/UserDepartment/controllers/UserController.java
@@ -2,8 +2,16 @@ package br.com.UserDepartment.controllers;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,4 +31,21 @@ public class UserController {
 		return result;
 	}
 
+	@GetMapping(value = "{id}")
+	public User findById(@PathVariable Long id) {
+		User result = repository.findById(id).get();
+		return result;
+	}
+
+	@PostMapping
+	public User insert(@RequestBody User user) {
+		User result = repository.save(user);
+		return result;
+	}
+	
+	
+	@DeleteMapping(value = "{id}")
+	public void delete(@PathVariable Long id) {
+		repository.delete(findById(id));
+	}
 }
