65e74b5530ea07d68305ba974744a8a9bb220e94
Gabriel <gabriel.c.zvarum@gmail.com>
2022-06-21 13:31:02 -0300

    Implementação da classe DepartmentController

--- src/main/java/br/com/UserDepartment/controllers/DepartmentController.java
+++ src/main/java/br/com/UserDepartment/controllers/DepartmentController.java
@@ -2,53 +2,43 @@ package br.com.UserDepartment.controllers;
 
 import java.util.List;
 
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.UserDepartment.entities.User;
-import br.com.UserDepartment.repositories.UserRepository;
+import br.com.UserDepartment.entities.Department;
+import br.com.UserDepartment.repositories.DepartmentRepository;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserController {
-
+@RequestMapping(value = "/department")
+public class DepartmentController {
+	
 	@Autowired
-	private UserRepository repository;
-
+	private DepartmentRepository repository;
+	
 	@GetMapping
-	public List<User> findAll() {
+	public List<Department> findAll() {
 		return repository.findAll();
-		
 	}
-
+	
 	@GetMapping(value = "{id}")
-	public User findById(@PathVariable Long id) {
+	public Department findById(@PathVariable Long id) {
 		return repository.findById(id).get();
-		
 	}
-
+	
 	@PostMapping
-	public User save(@RequestBody User user) {
-		return repository.save(user);
+	public Department save(@RequestBody Department department){
+		return repository.save(department);
 	}
-
-	@PutMapping(value = "{id}")
-	public User update(@PathVariable Long id, @RequestBody User user) {
-		User result = repository.findById(id).get();
-		BeanUtils.copyProperties(user, result, "id");
-		return repository.save(result);
-	}
-
+	
 	@DeleteMapping(value = "{id}")
 	public void delete(@PathVariable Long id) {
 		repository.delete(findById(id));
 	}
+
 }
